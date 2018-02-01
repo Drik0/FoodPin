4de2bb7d67a8c43bc8132a3ec72220b65a88803f
Gerson <gerson.lcc@hotmail.com>
2018-02-01 10:06:39 +0000

    Image for restaurant

--- FoodPin/RestaurantTableTableViewController.swift
+++ FoodPin/RestaurantTableTableViewController.swift
@@ -14,15 +14,18 @@ class RestaurantTableTableViewController: UITableViewController {
         let cellIdentifier = "Cell"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
         cell.textLabel?.text = restaurantName[indexPath.row]
-        cell.imageView?.image = UIImage(named: "restaurant.jpg")
+        cell.imageView?.image = UIImage(named: restaurantImages[indexPath.row])
         return cell
     }
+    
+    var restaurantImages = ["barrafina", "bourkestreetbakery", "cafedeadend", "cafeloisl", "cafelore", "caskpubkitchen", "confessional", "donostia", "fiveleaves", "forkeerestaurant", "grahamavenuemeats", "haighschocolate", "homei", "palominoespresso", "petiteoyster", "posatelier", "royaloak", "teakha", "traif", "upstate", "wafflewolf"]
     
     var restaurantName = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        restaurantName.sort()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
