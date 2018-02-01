50529de5aa61c12be0cf54e149a4d8b73712db64
Gerson <gerson.lcc@hotmail.com>
2018-02-01 10:51:10 +0000

    Prototype Cell Data Model

---
+++ FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by Gerson Costa on 01/02/2018.
+//  Copyright © 2018 Gerson Costa. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
