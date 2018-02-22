ae4d31371e88f4d2e53658d648f5603a98df7309
Gerson <gerson.lcc@hotmail.com>
2018-02-22 13:43:03 +0000

    Growing Animation ReviewView

--- FoodPin/Controller/ReviewViewController.swift
+++ FoodPin/Controller/ReviewViewController.swift
@@ -11,22 +11,22 @@ import UIKit
 class ReviewViewController: UIViewController {
     
     @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var containerView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        containerView.transform = CGAffineTransform(scaleX: 0, y: 0)
+        
         let blurEffect = UIBlurEffect(style: .dark)
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = view.bounds
         backgroundImageView.addSubview(blurEffectView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 0.3, animations: {self.containerView.transform = CGAffineTransform.identity})
+    }
     /*
     // MARK: - Navigation
 
